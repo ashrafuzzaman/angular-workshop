5c80a2d744ccfa9810e9ebbf9cc1ce86f39777b8
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-06-16 15:48:15 +0600

    Working with controller

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,10 @@
+function SimpleCrtl($scope) {
+    $scope.users = [
+        {name: 'Muntasim'},
+        {name: 'Jitu'},
+        {name: 'Bazlur'},
+        {name: 'Younus'},
+        {name: 'Rakib'},
+        {name: 'Sharvi'}
+    ]
+}
