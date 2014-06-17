5cf405ddc4f0b5fb930070789995597ed46fdad9
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-06-17 10:39:02 +0600

    Added user service

--- express-service/routes/users.js
+++ express-service/routes/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/', function (req, res) {
+    var users = [
+        {name: 'Muntasim', city: 'Dhaka'},
+        {name: 'Jitu', city: 'Dhaka'},
+        {name: 'Bazlur', city: 'Dhaka'},
+        {name: 'Younus', city: 'Dhaka'},
+        {name: 'Rakib', city: 'Dhaka'},
+        {name: 'Sharvi', city: 'Dhaka'}
+    ];
+    res.send(users);
 });
 
 module.exports = router;
