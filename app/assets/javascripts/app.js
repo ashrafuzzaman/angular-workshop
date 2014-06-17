e094eb9ea0530c915dd04bd2b77b98957c87e8ee
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-06-17 10:57:15 +0600

    Added user service

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,16 +1,14 @@
-var demoApp = angular.module('demoApp', []);
-
+var demoApp = angular.module('demoApp', ['ngResource']);
 var controllers = {};
 
-controllers.SimpleCrtl = function ($scope) {
-    $scope.users = [
-        {name: 'Muntasim', city: 'Dhaka'},
-        {name: 'Jitu', city: 'Dhaka'},
-        {name: 'Bazlur', city: 'Dhaka'},
-        {name: 'Younus', city: 'Dhaka'},
-        {name: 'Rakib', city: 'Dhaka'},
-        {name: 'Sharvi', city: 'Dhaka'}
-    ]
+demoApp.factory('userFactory', function ($resource) {
+    return {
+        users: $resource('http://localhost:3000/users')
+    };
+});
+
+controllers.SimpleCrtl = function ($scope, userFactory) {
+    $scope.users = userFactory.users.query();
 };
 
 demoApp.controller('SimpleCrtl', controllers.SimpleCrtl);
--- express-service/routes/users.js
+++ express-service/routes/users.js
@@ -4,12 +4,12 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function (req, res) {
     var users = [
-        {name: 'Muntasim', city: 'Dhaka'},
-        {name: 'Jitu', city: 'Dhaka'},
-        {name: 'Bazlur', city: 'Dhaka'},
-        {name: 'Younus', city: 'Dhaka'},
-        {name: 'Rakib', city: 'Dhaka'},
-        {name: 'Sharvi', city: 'Dhaka'}
+        {id: 1, name: 'Muntasim', city: 'Dhaka'},
+        {id: 2, name: 'Jitu', city: 'Dhaka'},
+        {id: 3, name: 'Bazlur', city: 'Dhaka'},
+        {id: 4, name: 'Younus', city: 'Dhaka'},
+        {id: 5, name: 'Rakib', city: 'Dhaka'},
+        {id: 6, name: 'Sharvi', city: 'Dhaka'}
     ];
     res.send(users);
 });
