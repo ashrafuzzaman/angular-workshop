aae73c8772624e2fe3847c1cefd712cc3218a39e
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-06-19 10:17:50 +0600

    Added list and detail of user

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,25 +1,42 @@
 var demoApp = angular.module('demoApp', ['ngResource', 'ngRoute']);
 var controllers = {};
 
-demoApp.config(['$routeProvider',
-    function ($routeProvider) {
-        $routeProvider.
-            when('/', {
-                templateUrl: 'partials/users.html',
-                controller: 'UsersCtrl'
-            }).otherwise({
-                redirectTo: '/'
-            });
-    }]);
-
-demoApp.factory('userFactory', function ($resource) {
+demoApp.factory('usersFactory', function ($resource) {
     return {
         users: $resource('/users')
     };
 });
 
-controllers.UsersCtrl = function ($scope, userFactory) {
-    $scope.users = userFactory.users.query();
+demoApp.factory('userFactory', function ($resource) {
+    return $resource('/users/:id', {}, {
+        get: { method: 'GET' },
+        update: { method: 'PUT', params: {id: '@id'} },
+        delete: { method: 'DELETE', params: {id: '@id'} }
+    })
+});
+
+demoApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/users', {
+                templateUrl: 'partials/users.html',
+                controller: 'UsersCtrl'
+            }).when('/users/:id', {
+                templateUrl: 'partials/user.html',
+                controller: 'UserDetailCtrl'
+            }).otherwise({
+                redirectTo: '/users'
+            });
+    }]);
+
+controllers.UsersCtrl = function ($scope, usersFactory) {
+    $scope.users = usersFactory.users.query();
+};
+
+controllers.UserDetailCtrl = function ($scope, $routeParams, userFactory) {
+    $scope.user = userFactory.get({id: $routeParams.id});
+    console.log($scope.user);
 };
 
 demoApp.controller('UsersCtrl', controllers.UsersCtrl);
+demoApp.controller('UserDetailCtrl', controllers.UserDetailCtrl);
